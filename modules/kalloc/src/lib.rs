d3b6b13e8dd73f9eaca12ea642d4da237ba86909
yufeng <321353225@qq.com>
2023-03-31 14:56:22 +0800

    support kernel alloc

---
+++ modules/kalloc/src/lib.rs
@@ -0,0 +1,20 @@
+#![no_std]
+
+use buddy_system_allocator::LockedHeap;
+
+// 堆大小
+const HEAP_SIZE: usize = 0x0008_0000;
+
+// 堆空间
+static mut HEAP: [u8;HEAP_SIZE] = [0;HEAP_SIZE];
+
+// 堆内存分配器
+#[global_allocator]
+static HEAP_ALLOCATOR: LockedHeap<64> = LockedHeap::empty();
+
+// 初始化堆内存分配器
+pub fn init() {
+    unsafe {
+        HEAP_ALLOCATOR.lock().init(HEAP.as_ptr() as usize, HEAP_SIZE);
+    }
+}
