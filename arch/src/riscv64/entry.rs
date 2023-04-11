7dc0f1a46cc8d12ef7a5e5765e51a857e6414938
yufeng <321353225@qq.com>
2023-04-12 00:42:55 +0800

    fix link and virt address

--- arch/src/riscv64/entry.rs
+++ arch/src/riscv64/entry.rs
@@ -1,3 +1,4 @@
+use crate::VIRT_ADDR_START;
 use crate::{PTEFlags, PAGE_ITEM_COUNT, PTE};
 
 /// 汇编入口函数
@@ -7,7 +8,7 @@ use crate::{PTEFlags, PAGE_ITEM_COUNT, PTE};
 #[no_mangle]
 #[link_section = ".text.entry"]
 unsafe extern "C" fn _start() -> ! {
-    const STACK_SIZE: usize = 4096 * 4 * 8;
+    const STACK_SIZE: usize = 0x10000;
 
     #[link_section = ".bss.stack"]
     static mut STACK: [u8; STACK_SIZE] = [0u8; STACK_SIZE];
@@ -16,40 +17,47 @@ unsafe extern "C" fn _start() -> ! {
     static mut PAGE_TABLE: [PTE; PAGE_ITEM_COUNT] = {
         let mut arr: [PTE; PAGE_ITEM_COUNT] = [PTE::new(); PAGE_ITEM_COUNT];
         // 初始化页表信息
-        // 0x00000000_00000000 -> 0x00000000 (1G)
         // 0x00000000_80000000 -> 0x80000000 (1G)
-        // 0xffffffff_c0000000 -> 0x80000000 (1G)
-        // 0xffffffff_40000000 -> 0x00000000 (1G)
+        // 高半核
+        // 0xffffffc0_00000000 -> 0x00000000 (1G)
+        // 0xffffffc0_80000000 -> 0x80000000 (1G)
         arr[2] = PTE::from_addr(0x8000_0000, PTEFlags::VRWX);
-        arr[509] = PTE::from_addr(0x0, PTEFlags::GVRWX);
-        arr[511] = PTE::from_addr(0x8000_0000, PTEFlags::GVRWX);
+        arr[0x100] = PTE::from_addr(0x0000_0000, PTEFlags::GVRWX);
+        arr[0x102] = PTE::from_addr(0x8000_0000, PTEFlags::GVRWX);
         arr
     };
 
     core::arch::asm!(
         // 1. 设置栈信息
         // sp = bootstack + (hartid + 1) * 0x10000
-        "   add     t0, a0, 1
-            slli    t0, t0, 14
-            lui     sp, %hi({stack})
-            add     sp, sp, t0 ",
+        "
+            la      sp, {boot_stack}
+            li      t0, {stack_size}
+            add     sp, sp, t0              // set boot stack
+
+            li      s0, {virt_addr_start}   // add virtual address
+            add     sp, sp, s0
+        ",
         // 2. 开启分页模式
         // satp = (8 << 60) | PPN(page_table)
-        "   lui     t0, %hi({page_table})
-            li      t1, 0xffffffffc0000000 - 0x80000000
-            sub     t0, t0, t1
+        "
+            la      t0, {page_table}
             srli    t0, t0, 12
             li      t1, 8 << 60
             or      t0, t0, t1
             csrw    satp, t0
-            sfence.vma",
-        // 3. 跳到 rust_main 函数，绝对路径
-        "   lui     t0, %hi(rust_main)
-            addi    t0, t0, %lo(rust_main)
-            jr      t0
+            sfence.vma
         ",
-        stack      = sym STACK,
+        // 3. 跳到 rust_main 函数，绝对路径
+        "
+            la      a2, rust_main
+            add     a2, a2, s0
+            jalr    a2                      // call rust_main
+        ",
+        stack_size = const STACK_SIZE,
+        boot_stack = sym STACK,
         page_table = sym PAGE_TABLE,
+        virt_addr_start = const VIRT_ADDR_START,
         options(noreturn),
     )
 }
