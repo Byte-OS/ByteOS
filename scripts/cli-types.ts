ed2342215c939f642b509cd9508f5c9a26256e25
yfblock <321353225@qq.com>
2024-11-01 02:39:34 +0800

    feat: trying to use deno as build script.

---
+++ scripts/cli-types.ts
@@ -0,0 +1,9 @@
+import { EnumType } from "https://deno.land/x/cliffy@v1.0.0-rc.4/command/mod.ts";
+
+export const logLevelEnum = new EnumType(["debug", "info", "warn", "error"]);
+export const archEnum = new EnumType(['x86_64', "aarch64", "riscv64", "loongarch64"]);
+
+export type globalArgType = { 
+    logLevel: typeof logLevelEnum, 
+    arch: typeof archEnum 
+};
